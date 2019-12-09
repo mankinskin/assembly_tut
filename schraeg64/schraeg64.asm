e34c485a3bb9c3c71610806f84d19c1ec8ffb93b
Linus Behrbohm <linusbehrbohm@web.de>
2019-12-09 17:52:44 +0100

    Continue schraeg64

--- schraeg64/schraeg64.asm
+++ schraeg64/schraeg64.asm
@@ -6,10 +6,11 @@
 
 ;;; start of data section
 section .data
-msg		db	'Hello World!'
+newline		db	0x0a
+space		db	0x20
 ;;; a newline character
-newline:
- 	db 0x0a
+;newline:
+; 	db 0x0a
 
 ;;; start of code section
 section	.text
@@ -20,60 +21,81 @@ section	.text
 ;;; main function
 _start:
 	pop rbx		; pop argument count into rbx (>= 1 guaranteed)
+	dec rbx
+	pop rsi   ; drop first argument (command name)
+
+	push rax
+	mov	rax, SYS_WRITE	; set syscall to SYS_WRITE
+	push rdi
+	mov	rdi, STDOUT		; write to STDOUT
+
+	push rdx ; write call length
+	mov rdx, 1 ; set length to 1
 read_args:
 	;; read next argument
 	pop rsi   ; pop address to next argument into rsi
-	call write_string
+	call write_arg
+	dec rbx
+	jnz read_args
+
+	pop rdx
+	pop rdi
+	pop rax
 
 exit:
 	;; exit program via syscall exit (necessary!)
-	mov	rax, SYS_EXIT	; set syscall to SYS_EXIT
-	mov	rdi, 0		; exit code 0 (= "ok")
-	syscall
-write_newline:
 	push rax
-	mov rax, SYS_WRITE
-
+	mov	rax, SYS_EXIT	; set syscall to SYS_EXIT
 	push rdi
-	mov	rdi, STDOUT		; write to STDOUT
+	mov	rdi, 0				; set exit code 0
+	syscall
+	pop rdi
+	pop rax
 
-	push rdx ; write call length
-	mov rdx, 1 ; set length to 0
+write_newline:
 	push rsi ; argument string address pointer
 	mov rsi, newline
-	syscall ; call SYS_WRITE to STDOUT with calculated length (in rdx)
+	syscall
 	pop rsi ; restore starting address of string
-	pop rdx
-	pop rdi
-	pop rax
+	ret
 
-;;; write string function
-write_string:
-	; remember values in registers we use
-	; prepare write call
-	push rax
-	mov	rax, SYS_WRITE	; set syscall to SYS_WRITE
+;;; write string schraeg function
+;; writes string slanted in rsi to stdout
+write_arg:
+	push rcx		; char count
+	mov rcx, 0	; set char count to 0
 
-	push rdi
-	mov	rdi, STDOUT		; write to STDOUT
+	push rsi ; remember string address
+	; rsi points to current char
 
-	push rdx ; write call length
-	mov rdx, 0 ; set length to 0
+walk_string:				; loop
+	cmp rsi, byte 0 ; zero byte (eos) reached?
+	je reached_end		; yes, finish >>>
 
-	push rsi ; argument string address pointer
-search_eos:
-	cmp [rsi], byte 0 ; zero byte (eos) reached?
-	je found_eos ; yes, findish
-	; else
-	inc rdx ; count length
-	inc rsi ; next byte
-	jmp search_eos ; loop
-found_eos:
+;;; first, write spaces
+;	push rsi				; remember current char
+;	mov rsi, space	; current char -> space
+;	push rcx				; char count -> white spaces remaining
+;write_spaces:				; loop
+;	cmp [rcx], byte 0 ; zero byte (eos) reached?
+;	je finish_spaces	; yes, finish >>>
+;	syscall						; write 1 space to stdout
+;	dec rcx						; dec whitespace counter
+;	jmp write_spaces	; loop ^^^
+;finish_spaces:
+;	pop rcx ; 0 -> char count
+;	pop rsi ; space -> current char
+write_char:
+	syscall ; write 1 char to stdout
+	inc rcx ; increment char count
+	inc rsi ; next char
+finish_char:
+	call write_newline
+	jmp walk_string ; loop ^^^
+
+reached_end:
 	; pop remembered values back into registers
 	; opposite order because of stack
-	pop rsi ; restore starting address of string
-	syscall ; call SYS_WRITE to STDOUT with calculated length (in rdx)
-	pop rdx
-	pop rdi
-	pop rax
+	pop rsi ; eos -> string
+	pop rcx ; char count -> _
 	ret
