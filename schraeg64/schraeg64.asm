d21badbbfea38d94b47d31a38aeecec182b95bb1
Linus Behrbohm <linusbehrbohm@web.de>
2019-12-16 19:29:44 +0100

    Fix corruption of counter in rcx caused by syscall

--- schraeg64/schraeg64.asm
+++ schraeg64/schraeg64.asm
@@ -9,7 +9,7 @@ section .data
 ;;; a newline character
 newline		db	0x0a
 ;;; a space character
-space		db	0x20
+space		db	' '
 
 ;;; start of code section
 section	.text
@@ -22,6 +22,10 @@ _start:
 	pop rbx		; pop argument count into rbx (>= 1 guaranteed)
 	pop rsi   ; drop first argument (command name)
 
+	mov	rax, SYS_WRITE	; set syscall to SYS_WRITE
+	mov	rdi, STDOUT			; write to STDOUT
+	mov rdx, 1					; set length to 1
+
 read_args:
 	;; read next argument
 	dec rbx   
@@ -30,19 +34,18 @@ read_args:
 	call write_arg
 	jmp read_args
 
-	pop rdx
-	pop rdi
-	pop rax
-
 exit:
 	;; exit program via syscall exit (necessary!)
-	push rax
 	mov	rax, SYS_EXIT	; set syscall to SYS_EXIT
-	push rdi
 	mov	rdi, 0				; set exit code 0
 	syscall
-	pop rdi
-	pop rax
+
+write_char:
+	push rcx
+	; syscall is configured at _start
+	syscall
+	pop rcx
+	ret
 
 write_newline:
 	push rsi ; argument string address pointer
@@ -50,57 +53,41 @@ write_newline:
 	call write_char
 	pop rsi ; restore starting address of string
 	ret
-write_char:
-	push rax
-	mov	rax, SYS_WRITE	; set syscall to SYS_WRITE
-	push rdi
-	mov	rdi, STDOUT			; write to STDOUT
-	push rdx
-	mov rdx, 1					; set length to 1
-	syscall
-	pop rdx
-	pop rdi
-	pop rax
-	ret
 
 ;;; write string schraeg function
-;; writes string slanted in rsi to stdout
+;; writes string in rsi slanted to stdout
 write_arg:
-	push rcx
-	mov rcx, 0	; set char count to 0
-
 	push rsi ; remember string address
+	mov rcx, 0	; set char count to 0
 	; rsi points to current char
 walk_string:				; loop
 	cmp [rsi], byte 0 ; zero byte (eos) reached?
 	je reached_end		; yes, finish >>>
 
-write_spaces:				; loop
+write_spaces:				
+	push rcx					; char count -> white spaces remaining
   push rsi					; remember current char
 	mov rsi, space		; current char -> space
-	push rcx					; char count -> white spaces remaining
-write_space:
+
+loop_spaces:        ; loop
 	cmp rcx, byte 0		; 0 reached?
 	je finish_spaces	; yes, finish >>>
-	dec rcx						; dec whitespace counter
-	call write_char						; write 1 space to stdout
-	jmp write_space		; loop ^^^
+write_space:
+	call write_char		; write 1 space to stdout
+	dec rcx						; decrement whitespace counter
+	jmp loop_spaces		; loop ^^^
+
 finish_spaces:
-	pop rcx ; 0 -> char count
 	pop rsi ; space -> current char
+	pop rcx ; 0 -> char count
 
 finish_char:
-	push rcx
 	call write_char ; write 1 char to stdout
 	call write_newline
-	pop rcx
-	inc rcx ; increment char count
 	inc rsi ; next char
+	inc rcx ; increment char count
 	jmp walk_string ; loop ^^^
 
 reached_end:
 	pop rsi ; eos -> string
-	pop rcx ; char count -> _
-	; pop remembered values back into registers
-	; opposite order because of stack
 	ret
